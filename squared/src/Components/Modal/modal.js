692c65e7b0b0c77c78176cccfee1b1f12bc53ce2
Bianchi Mena <Bianchimena1999@gmail.com>
2023-08-25 15:02:19 -0700

    Update modal.js

--- squared/src/Components/Modal/modal.js
+++ squared/src/Components/Modal/modal.js
@@ -1,11 +1,16 @@
-import react from 'react'
+import {react, useState} from 'react'
 
-function modal() {
+function Modal() {
+// 1. create an x that allows you to click out of the game (on the topright of the modal)
+// 2. create a div or label that houses the question for example 10 to the power of 5
+// 3. create an input/label that allows the user to enter a value
 
-    <div className="container">
-        
+    const [toggleModal, setToggleModal] = useState(false);
+
+    <div className="modal">
+        <button>Modal</button>
     </div>
 
 }
 
-export default modal
+export default Modal
