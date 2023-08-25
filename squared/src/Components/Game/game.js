8657b7e5367e0fa49d1c8d81ba1a66b0b7b78b36
Bianchi Mena <Bianchimena1999@gmail.com>
2023-08-25 16:06:20 -0700

    added x icon and shell of game

--- squared/src/Components/Game/game.js
+++ squared/src/Components/Game/game.js
@@ -2,6 +2,19 @@ import {useState} from 'react'
 
 function Game() {
 
+return (
+<div>
+    <div className="row">
+        <i class="fa-solid fa-xmark" id="xmark"></i>
+    </div>
+    <div className="row">
+        <div className="question"></div>
+    </div>
+    <div className="row">
+        <input></input>
+    </div>
+</div>
+)
 
 }
 
