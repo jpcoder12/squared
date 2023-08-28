e7ac07fad79928ae217c13e42d89c9756006ce12
Joshua Peoples <jpcoder12@gmail.com>
2023-08-28 14:36:17 -0700

    change format of home

--- squared/src/Components/Home/home.js
+++ squared/src/Components/Home/home.js
@@ -15,46 +15,49 @@ function Home(){
             console.log('Modal is down', showModal)
         }
     }
-   
     
     return(
-        <div className='container'>
-        
-            {showModal ? <Game playGame={playGame} difficulty={difficulty} timed={timed} /> : 
-            <div className='container' id="home">
+        <div className='container' id="home">
             <div className="row mt-4 mb-4" id="title-row">
                 <h1>SQUARED</h1>
             </div>
-            Choose your preferences below and then click 'Start'
-            <div className="row my-4" id="diff-row">
-                <div className='col-3 mx-3 btn btn-primary'>Easy</div>
-                <div className='col-3 mx-3 btn btn-success'>Medium</div>
-                <div className='col-3 mx-3 btn btn-warning'>Hard</div>
-                <div className='col-3 mx-3 btn btn-danger'>Master</div>
-            </div>
-            <div className="row my-4" id="timing-row">
-                <div className='col-6'>
-                    <div className="btn btn-secondary">
-                        Time Limit
+            <div className='row'>
+                <div className='col'>
+                    
+                    Choose your preferences below and then click 'Start'
+                    <div className="row my-4" id="diff-row">
+                        <div className='col-3 mx-1 btn btn-primary'>Easy</div>
+                        <div className='col-3 mx-1 btn btn-success'>Medium</div>
+                        <div className='col-3 mx-1 btn btn-warning'>Hard</div>
+                        <div className='col-3 mx-1 btn btn-danger'>Master</div>
+                    </div>
+                    <div className="row my-4" id="timing-row">
+                        <div className='col-6'>
+                            <div className="btn btn-secondary">
+                                Time Limit
+                            </div>
+                        </div>
+                        <div className='col-6'>
+                            <div className="btn btn-secondary" id="endless-button">
+                                Endless
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row justify-content-center my-4'>
+                        <div className="row">
+                            <div  id="start-button" className='btn btn-light' onClick={playGame}>Start</div>
+                        </div>
                     </div>
                 </div>
-                <div className='col-6'>
-                    <div className="btn btn-secondary" id="endless-button">
-                        Endless
-                    </div>
+                <div className="col" id="game">
+                    <Game />
                 </div>
             </div>
-            <div className='row justify-content-center my-4'>
-                <div className="row">
-                    <div  id="start-button" className='btn btn-light' onClick={playGame}>Start</div>
-                </div>
-                <div className="my-2" id="footer">
-                    <span>Made with ❤️ by </span>
-                    <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/joshpeoples/"> Joshua Peoples</a> <span>and</span>
-                    <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/bianchimena/"> Bianchi Mena</a>
-                </div>
-            </div></div>}
-            
+            <div className="my-2" id="footer">
+                <span>Made with ❤️ by </span>
+                <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/joshpeoples/"> Joshua Peoples</a> <span>and</span>
+                <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/bianchimena/"> Bianchi Mena</a>
+            </div>
         </div>
     )
 }
