2eabf316dff988d817582a4dcd15e68bd16da487
Bianchi Mena <Bianchimena1999@gmail.com>
2023-08-28 15:19:57 -0700

    game props

--- squared/src/Components/Game/game.js
+++ squared/src/Components/Game/game.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 
-function Game() {
+function Game(props) {
 
     const [questions, setQuestions] = useState([]);
     const [answer, setAnswer] = useState(""); 
@@ -12,7 +12,7 @@ function Game() {
 return (
     <div id="game">
         <div className="row">
-            <div id="question">Question</div>
+            <div id="question">{props.difficulty}</div>
         </div>
         <div className="row">
             <div id="answer">Answer</div>
