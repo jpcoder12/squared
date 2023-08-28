6e40bdd0c21b7b15dee7d0a6e86f648c8074d3cb
Joshua Peoples <jpcoder12@gmail.com>
2023-08-28 14:49:42 -0700

    remove id game from home

--- squared/src/Components/Home/home.js
+++ squared/src/Components/Home/home.js
@@ -49,7 +49,7 @@ function Home(){
                         </div>
                     </div>
                 </div>
-                <div className="col" id="game">
+                <div className="col">
                     <Game />
                 </div>
             </div>
